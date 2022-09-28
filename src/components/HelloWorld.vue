cb2175bca89761f94ebcea381611e6d48e0b0ef3
meme2wsx <meme2wsx@gmail.com>
2022-09-29 00:27:56 +0900

    クリックしたら数字がインクリメントされるようなプログラム

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
 <h1 class="card-header">{{ message }}</h1>
 <p>TypeScriptとVue.jsで書きました</p>
+<h2 v-on:click="doAction">クリックしてね</h2>
+<h3>{{ val }} </h3>
 </template>
 
 <script lang="ts">
@@ -8,5 +10,10 @@ import { Options, Vue } from 'vue-class-component';
 
 export default class HelloWorld extends Vue {
   message = "サンプルウェブページ"
+  val = 1
+
+  doAction():void{
+    this.val += 1
+  }
 }
 </script>
